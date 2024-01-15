238ebfcb14747d077b6662797de5c8724caa9fd9
Piotr Adamowicz <adamowicz.piotr2@gmail.com>
2024-01-15 14:29:53 +0100

    drag and drop function added

--- src/components/TailList.vue
+++ src/components/TailList.vue
@@ -1,13 +1,16 @@
 <script lang="ts" setup>
 import Tail from './Tail.vue'
 import { useStore } from '@/stores/store'
+import draggable from 'vuedraggable'
 
 const store = useStore()
 </script>
 <template>
-  <ul class="tailList">
-    <Tail v-for="tail in store.tails" :tail="tail" :key="tail.id" />
-  </ul>
+  <draggable v-model="store.tails" tag="ul" group="tails" item-key="id">
+    <template #item="{ element: tail }">
+      <Tail :tail="tail" :key="tail.id" />
+    </template>
+  </draggable>
 </template>
 
 <style scoped lang="scss">
